2d6f678e306b564ab4ba238f22c22c51012219e0
carloseam94 <carloseam94@gmail.com>
2025-09-11 14:25:52 +0200

    add: tooltip on feature click

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 import Map from 'ol/Map';
-import * as olHelpers from '@/utils/ol-helpers';
+import {
+    createMap, addNfzOverlay, addNoFlyZones
+} from '@/utils/ol-helpers';
 import 'ol/ol.css';
 
 const map = ref<Map | null>(null);
@@ -9,18 +11,47 @@ const map = ref<Map | null>(null);
 const BRIGHTLANDS_CAMPUS_COORDS = [5.97311012907296, 50.883308586237696]; // [longitude, latitude]
 
 onMounted(() => {
-    map.value = olHelpers.createMap('map', BRIGHTLANDS_CAMPUS_COORDS, 16);
+    // create map
+    map.value = createMap('map', BRIGHTLANDS_CAMPUS_COORDS, 16);
+
+    if (!map) {
+        throw new Error('Invalid Map')
+    }
+
+    // add no fly zones
+    // taken from https://www.openaip.net/data/exports?page=1&limit=50&sortBy=country&sortDesc=true&failed=false&country=NL&format=geojson
+    const noFlyZones = [
+        'nl_apt',
+        // 'nl_asp',
+        'nl_hot',
+        'nl_nav',
+        'nl_obs',
+        'nl_raa',
+        'nl_rca',
+        'nl_rpp',
+    ]
+    addNoFlyZones(map.value as Map, noFlyZones);
+
+    // add info tooltip to nfz when clicked
+    addNfzOverlay(map.value as Map, 'overlayPopup');
 });
 
 </script>
 
 <template>
     <div id="map"></div>
+    <div id="overlayPopup" class="ol-popup"></div>
 </template>
 
 <style scoped>
 #map {
     width: 100%;
     height: 100vh;
+}
+
+#overlayPopup {
+    background-color: white;
+    padding: 8px;
+    border-radius: 10px;
 }
 </style>
--- src/utils/ol-helpers.ts
+++ src/utils/ol-helpers.ts
@@ -4,8 +4,12 @@ import * as Source from 'ol/source'
 import * as Layer from 'ol/layer'
 import GeoJSON from 'ol/format/GeoJSON'
 import { Style, Stroke, Fill, Circle } from 'ol/style'
+import Overlay from 'ol/Overlay'
 import type { Coordinate } from 'ol/coordinate'
 import { fromLonLat } from 'ol/proj'
+import type RenderFeature from 'ol/render/Feature'
+import { Feature } from 'ol'
+import { Geometry, LineString, Point, Polygon } from 'ol/geom'
 
 export function createMap(
   target: string = 'map',
@@ -30,19 +34,11 @@ export function createMap(
     view,
   })
 
-  // taken from https://www.openaip.net/data/exports?page=1&limit=50&sortBy=country&sortDesc=true&failed=false&country=NL&format=geojson
-  const noFlyZones = [
-    'nl_apt',
-    // 'nl_asp',
-    'nl_hot',
-    'nl_nav',
-    'nl_obs',
-    'nl_raa',
-    'nl_rca',
-    'nl_rpp',
-  ]
+  return map
+}
 
-  noFlyZones.forEach((nfz) => {
+export function addNoFlyZones(map: Map, nfzList: string[]): void {
+  nfzList.forEach((nfz) => {
     const noFlyZonesVector = new Source.Vector({
       url: `data/nfz/${nfz}.geojson`,
       format: new GeoJSON({
@@ -77,6 +73,68 @@ export function createMap(
     // add no fly zones layers
     map.addLayer(noFlyZonesLayer)
   })
+}
 
-  return map
+export function addNfzOverlay(map: Map, elementId: string): void {
+  const popupElement = document.getElementById(elementId)
+
+  if (!popupElement) {
+    throw new Error('Overlay Popup not found')
+  }
+
+  const popup = new Overlay({
+    element: popupElement,
+    positioning: 'bottom-center',
+    stopEvent: true,
+    offset: [0, -10],
+  })
+
+  map.addOverlay(popup)
+
+  map.on('singleclick', (evt) => {
+    popupElement.replaceChildren()
+
+    let clickedFeature = map.forEachFeatureAtPixel(evt.pixel, (feature) => feature)
+
+    if (clickedFeature instanceof Feature) {
+      const geometry = clickedFeature?.getGeometry()
+
+      if (
+        geometry instanceof Point ||
+        geometry instanceof Polygon ||
+        geometry instanceof LineString
+      ) {
+        const coords = geometry.getCoordinates() as Coordinate
+        const props = clickedFeature.getProperties()
+
+        console.log(props)
+
+        const nameEle = document.createElement('b')
+        nameEle.innerText = props.name ?? 'No name'
+
+        popupElement.appendChild(nameEle)
+
+        const elevation = props.elevation?.value
+        if (elevation) {
+          const elevationEle = document.createElement('div')
+          elevationEle.innerText = `Elevation: ${elevation ?? '-1'}`
+          popupElement.appendChild(elevationEle)
+        }
+
+        const osmTags = props.osmTags
+
+        if (osmTags) {
+          Object.entries(osmTags).forEach(([key, value]) => {
+            const tagEle = document.createElement('div')
+            tagEle.innerText = `${key}: ${value}`
+            popupElement.appendChild(tagEle)
+          })
+        }
+
+        popup.setPosition(coords)
+      }
+    } else {
+      popup.setPosition(undefined) // hide popup if clicked outside
+    }
+  })
 }
